ffbfd5fa7202c5d041f2633db0faf00ec2d60564
Jimin0304 <pjws0304@naver.com>
2022-10-17 23:16:13 +0900

    initial commit

---
+++ src/main/java/com/opensource/weathercloset/WeatherclosetServerApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.weathercloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherclosetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherclosetServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensource/weathercloset/WeatherclosetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.weathercloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherclosetServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
