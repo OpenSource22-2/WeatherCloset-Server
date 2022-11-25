960eabdeb43f2e35f7e8a1e66e96c100bcce5bdb
Jimin0304 <pjws0304@naver.com>
2022-11-25 21:09:10 +0900

    refactor(weather): WeatherController 코드 리팩토링 (#9)

--- src/main/java/com/opensource/weathercloset/weather/controller/WeatherController.java
+++ src/main/java/com/opensource/weathercloset/weather/controller/WeatherController.java
@@ -1,26 +1,27 @@
 package com.opensource.weathercloset.weather.controller;
 
 import com.opensource.weathercloset.weather.domain.Weather;
-import com.opensource.weathercloset.weather.repository.WeatherRepository;
 import com.opensource.weathercloset.weather.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+import static java.net.URLEncoder.encode;
 import static org.springframework.http.HttpStatus.OK;
 
 
@@ -30,105 +31,45 @@ import static org.springframework.http.HttpStatus.OK;
 public class WeatherController {
 
     private final WeatherService weatherService;
-    private final WeatherRepository weatherRepository;
 
     @Value("${app.weatherApiToken}")
     private String WEATHER_API_TOKEN;
+    private static final String WEATHER_OPENAPI_URL = "http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList";
 
     @PostMapping("/api/parse")
     @ResponseStatus(OK)
     public ResponseEntity<Weather> addWeather() {
         try {
-                StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList"); /*URL*/
-                urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + WEATHER_API_TOKEN); /*Service Key*/
-                urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default : XML*/
-                urlBuilder.append("&" + URLEncoder.encode("dataCd", "UTF-8") + "=" + URLEncoder.encode("ASOS", "UTF-8")); /*자료 분류 코드(ASOS)*/
-                urlBuilder.append("&" + URLEncoder.encode("dateCd", "UTF-8") + "=" + URLEncoder.encode("DAY", "UTF-8")); /*날짜 분류 코드(DAY)*/
-                urlBuilder.append("&" + URLEncoder.encode("startDt", "UTF-8") + "=" + URLEncoder.encode("20221124", "UTF-8")); /*조회 기간 시작일(YYYYMMDD)*/
-                urlBuilder.append("&" + URLEncoder.encode("endDt", "UTF-8") + "=" + URLEncoder.encode("20221124", "UTF-8")); /*조회 기간 종료일(YYYYMMDD) (전일(D-1)까지 제공)*/
-                urlBuilder.append("&" + URLEncoder.encode("stnIds", "UTF-8") + "=" + URLEncoder.encode("108", "UTF-8")); /*종관기상관측 지점 번호 (활용가이드 하단 첨부 참조)*/
-                URL url = new URL(urlBuilder.toString());
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Content-type", "application/json");
+            JSONArray parse_item = getJsonArray();
 
-                System.out.println("Response code: " + conn.getResponseCode());
-                BufferedReader rd;
-                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                } else {
-                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-                }
-
-                String line;
-                String result = "";
-                while ((line = rd.readLine()) != null) {
-                    result = result.concat(line);
-                }
-
-                JSONParser parser = new JSONParser();
-                JSONObject obj = (JSONObject) parser.parse(result);
-
-                JSONObject parse_response = (JSONObject) obj.get("response");
-                JSONObject parse_body = (JSONObject) parse_response.get("body");
-                JSONObject parse_items = (JSONObject) parse_body.get("items");
-
-                JSONArray parse_item = (JSONArray) parse_items.get("item");
-
-                JSONObject weather;
+            JSONObject jsonWeather;
 
                 for (int i = 0; i < parse_item.size(); i++) {
-                    weather = (JSONObject) parse_item.get(i);
+                    jsonWeather = (JSONObject) parse_item.get(i);
 
                     // date 형으로 바꾸기 전 string 형
-                    String tm = (String) weather.get("tm");
+                    String tm = (String) jsonWeather.get("tm");
 
                     // float 형으로 바꾸기 전 string 형
-                    String strAvgTa = (String) weather.get("avgTa");
-                    String strMinTa = (String) weather.get("minTa");
-                    String strMaxTa = (String) weather.get("maxTa");
-                    String strSnow = (String) weather.get("ddMefs");
-                    String strRain = (String) weather.get("sumRn");
-                    String strCloud = (String) weather.get("avgTca");
+                    String strAvgTa = (String) jsonWeather.get("avgTa");
+                    String strMinTa = (String) jsonWeather.get("minTa");
+                    String strMaxTa = (String) jsonWeather.get("maxTa");
+                    String strSnow = (String) jsonWeather.get("ddMefs");
+                    String strRain = (String) jsonWeather.get("sumRn");
+                    String strCloud = (String) jsonWeather.get("avgTca");
 
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.KOREA);
                     LocalDate date = LocalDate.parse(tm, formatter);
 
-                    float avgTa = Float.parseFloat(strAvgTa);
-                    float minTa = Float.parseFloat(strMinTa);
-                    float maxTa = Float.parseFloat(strMaxTa);
+                    float avgTa = stringToFloat(strAvgTa);
+                    float minTa = stringToFloat(strMinTa);
+                    float maxTa = stringToFloat(strMaxTa);
+                    float snow = stringToFloat(strSnow);
+                    float rain = stringToFloat(strRain);
+                    float cloud = stringToFloat(strCloud);
+                    int icon_type = getIconType(snow, rain, cloud);
 
-                    float snow;
-                    if (strSnow.isEmpty())
-                        snow = 0.0f;
-                    else
-                        snow = Float.parseFloat(strSnow);
-
-                    float rain;
-                    if (strRain.isEmpty())
-                        rain = 0.0f;
-                    else
-                        rain = Float.parseFloat(strRain);
-
-                    float cloud;
-                    if (strCloud.isEmpty())
-                        cloud = 0.0f;
-                    else
-                        cloud = Float.parseFloat(strCloud);
-
-                    int icon_type;
-                    if (snow > 0)
-                        icon_type = 1; // 눈
-                    else if (snow == 0 && rain > 0)
-                        icon_type = 2; // 비
-                    else if (snow == 0 && rain == 0 && cloud >= 8.0)
-                        icon_type = 3; // 흐림
-                    else if (snow == 0 && rain == 0 && cloud >= 3.0)
-                        icon_type = 4; // 조금 흐림
-                    else
-                        icon_type = 5; // 맑음
-
-                    Weather createWeather = Weather.builder()
+                    Weather weather = Weather.builder()
                             .avgTa(avgTa)
                             .minTa(minTa)
                             .maxTa(maxTa)
@@ -140,15 +81,88 @@ public class WeatherController {
                             .build();
 
                     return ResponseEntity.ok(
-                            weatherService.addWeather(createWeather)
+                            weatherService.addWeather(weather)
                     );
                 }
-            rd.close();
-            conn.disconnect();
         } catch(Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private String callWeatherApi() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder(WEATHER_OPENAPI_URL)
+                .append("?" + encode("serviceKey", "UTF-8") + "=" + WEATHER_API_TOKEN) /*Service Key*/
+                .append("&" + encode("dataType", "UTF-8") + "=" + encode("JSON", "UTF-8")) /*요청자료형식(XML/JSON) Default : XML*/
+                .append("&" + encode("dataCd", "UTF-8") + "=" + encode("ASOS", "UTF-8")) /*자료 분류 코드(ASOS)*/
+                .append("&" + encode("dateCd", "UTF-8") + "=" + encode("DAY", "UTF-8")) /*날짜 분류 코드(DAY)*/
+                .append("&" + encode("startDt", "UTF-8") + "=" + encode("20221124", "UTF-8")) /*조회 기간 시작일(YYYYMMDD)*/
+                .append("&" + encode("endDt", "UTF-8") + "=" + encode("20221124", "UTF-8")) /*조회 기간 종료일(YYYYMMDD) (전일(D-1)까지 제공)*/
+                .append("&" + encode("stnIds", "UTF-8") + "=" + encode("108", "UTF-8")); /*종관기상관측 지점 번호 (활용가이드 하단 첨부 참조)*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        String line;
+        String result = "";
+        while ((line = rd.readLine()) != null) {
+            result = result.concat(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+        return result;
+    }
+
+    private JSONArray getJsonArray() throws IOException, ParseException {
+        String result;
+        result = callWeatherApi();
+
+        JSONParser parser = new JSONParser();
+        JSONObject obj = (JSONObject) parser.parse(result);
+
+        JSONObject parse_response = (JSONObject) obj.get("response");
+        JSONObject parse_body = (JSONObject) parse_response.get("body");
+        JSONObject parse_items = (JSONObject) parse_body.get("items");
+
+        JSONArray parse_item = (JSONArray) parse_items.get("item");
+        return parse_item;
+    }
+
+    private float stringToFloat(String stringItem) {
+        float floatItem;
+
+        if (stringItem.isEmpty())
+            floatItem = 0.0f;
+        else
+            floatItem = Float.parseFloat(stringItem);
+
+        return floatItem;
+    }
+
+    private static int getIconType(float snow, float rain, float cloud) {
+        int icon_type;
+
+        if (snow > 0)
+            icon_type = 1; // 눈
+        else if (snow == 0 && rain > 0)
+            icon_type = 2; // 비
+        else if (snow == 0 && rain == 0 && cloud >= 8.0)
+            icon_type = 3; // 흐림
+        else if (snow == 0 && rain == 0 && cloud >= 3.0)
+            icon_type = 4; // 조금 흐림
+        else
+            icon_type = 5; // 맑음
+
+        return icon_type;
     }
 
 }
--- src/main/java/com/opensource/weathercloset/weather/service/WeatherService.java
+++ src/main/java/com/opensource/weathercloset/weather/service/WeatherService.java
@@ -1,14 +1,13 @@
 package com.opensource.weathercloset.weather.service;
 
+import com.opensource.weathercloset.common.exception.ErrorCode;
+import com.opensource.weathercloset.common.exception.InvalidValueException;
 import com.opensource.weathercloset.weather.domain.Weather;
 import com.opensource.weathercloset.weather.repository.WeatherRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +16,8 @@ public class WeatherService {
     private final WeatherRepository weatherRepository;
 
     @Transactional
-    public Weather addWeather(final Weather createWeather) {
-        if (createWeather == null) throw new IllegalArgumentException("weather cannot be null");
-        return weatherRepository.save(createWeather);
+    public Weather addWeather(final Weather weather) {
+        if (weather == null) throw new InvalidValueException(ErrorCode.WEATHER_NOT_NULL);
+        return weatherRepository.save(weather);
     }
 }
