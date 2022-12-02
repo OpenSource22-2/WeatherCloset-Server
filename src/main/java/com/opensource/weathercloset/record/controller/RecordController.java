fd9119627ed819e5c681c2bde28dd73229c39b71
siyeonSon <siyeon0209@naver.com>
2022-12-02 17:52:11 +0900

    chore(record): RecordController path 수정 (#27)
    - @RequestMapping(value = "/record") 하여 모든 RecordController 의 API 들이 /record 로 시작하는 것보다, 각 API에 적합한 형태로 변경함
      - 예: 기존의 방식대로면 캘린더 정보를 알려주는 API의 경우 /record/calendar 이지만 변경 후에는 /calendar 로 사용할 수 있음.
      - 변경 사유: 프론트엔드가 요청 URL 을 통해서 API의 용도를 더 직관적으로 이해할 수 있을 것이라 판단하였기 때문

--- src/main/java/com/opensource/weathercloset/record/controller/RecordController.java
+++ src/main/java/com/opensource/weathercloset/record/controller/RecordController.java
@@ -1,6 +1,7 @@
 package com.opensource.weathercloset.record.controller;
 
 import com.opensource.weathercloset.common.dto.BasicResponse;
+import com.opensource.weathercloset.record.dto.CalendarRequestDTO;
 import com.opensource.weathercloset.record.dto.HeartUpdateRequestDTO;
 import com.opensource.weathercloset.record.dto.RecordRequestDTO;
 import com.opensource.weathercloset.record.dto.RecordUpdateRequestDTO;
@@ -18,7 +19,6 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
-@RequestMapping(value = "/record")
 @RequiredArgsConstructor
 @Tag(name = "record", description = "저장 API")
 public class RecordController {
@@ -35,7 +35,7 @@ public class RecordController {
         );
     }
 
-    @GetMapping("/{recordId}")
+    @GetMapping("/record/{recordId}")
     @ResponseStatus(OK)
     @Operation(summary = "기록 단건 조회", description = "기록을 단건 조회합니다")
     public ResponseEntity<BasicResponse> getRecord(@PathVariable("recordId") Long recordId) {
@@ -44,8 +44,18 @@ public class RecordController {
         );
     }
 
+    @GetMapping("/calendar/{memberId}")
+    @ResponseStatus(OK)
+    @Operation(summary = "사용자 월 기록 조회(캘린더)", description = "사용자의 월 기록을 조회합니다")
+    public ResponseEntity<BasicResponse> getCalendarInfo(@PathVariable("memberId") Long memberId,
+                                                         @RequestBody CalendarRequestDTO requestDTO) {
+        LocalDate recordDate = requestDTO.getRecordDate();
+        return basicResponse.ok(
+                recordService.getCalendarInfo(memberId, recordDate)
+        );
+    }
 
-    @PostMapping("/{memberId}")
+    @PostMapping("/record/{memberId}")
     @ResponseStatus(OK)
     @Operation(summary = "기록 등록", description = "기록을 신규 등록합니다")
     public ResponseEntity<BasicResponse> addRecord(@PathVariable("memberId") Long memberId,
@@ -61,7 +71,7 @@ public class RecordController {
         );
     }
 
-    @PutMapping("/{recordId}")
+    @PutMapping("/record/{recordId}")
     @ResponseStatus(NO_CONTENT)
     @Operation(summary = "기록 수정", description = "이미지, 별점, 한 줄 기록, 좋아요 여부, 날짜를 수정합니다")
     public ResponseEntity updateRecord(@PathVariable("recordId") Long recordId,
@@ -76,7 +86,7 @@ public class RecordController {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/like/{recordId}")
+    @PutMapping("/record/like/{recordId}")
     @ResponseStatus(NO_CONTENT)
     @Operation(summary = "좋아요 수정", description = "좋아요 여부를 수정합니다")
     public ResponseEntity updateHeart(@PathVariable("recordId") Long recordId,
@@ -87,7 +97,7 @@ public class RecordController {
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{recordId}")
+    @DeleteMapping("/record/{recordId}")
     @ResponseStatus(NO_CONTENT)
     @Operation(summary = "기록 삭제", description = "기록을 삭제합니다")
     public ResponseEntity deleteRecord(@PathVariable Long recordId) {
