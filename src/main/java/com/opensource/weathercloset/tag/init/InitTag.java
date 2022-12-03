26c1756d9f0c541003559816e06896b3b4496d67
siyeonSon <siyeon0209@naver.com>
2022-12-03 17:29:33 +0900

    chore(tag): @PostConstruct 주석 처리 (#28)
    - DB의 값들을 모두 DROP하여 태그 정보를 불러오고 싶을 때만 주석을 풀고 tag 테이블의 값을 업데이트하면 됨

--- src/main/java/com/opensource/weathercloset/tag/init/InitTag.java
+++ src/main/java/com/opensource/weathercloset/tag/init/InitTag.java
@@ -18,7 +18,7 @@ public class InitTag {
     private static final List<String> tagNames = List.of("더워요", "따뜻해요", "습해요", "일교차가 커요", "선선해요", "쌀쌀해요", "건조해요", "바람이 불어요", "햇빛이 강해요");
     private final TagRepository tagRepository;
 
-    @PostConstruct
+//    @PostConstruct
     public void init() {
         List<Tag> tags = tagNames.stream()
                 .map(Tag::new)
