b714d658f5cd89dc07ce50c27e364f2866348125
siyeonSon <siyeon0209@naver.com>
2022-12-05 18:43:40 +0900

    chore(tag): tag 추가 (#39)

--- src/main/java/com/opensource/weathercloset/tag/init/InitTag.java
+++ src/main/java/com/opensource/weathercloset/tag/init/InitTag.java
@@ -5,7 +5,6 @@ import com.opensource.weathercloset.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,7 +14,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class InitTag {
 
-    private static final List<String> tagNames = List.of("더워요", "따뜻해요", "습해요", "일교차가 커요", "선선해요", "쌀쌀해요", "건조해요", "바람이 불어요", "햇빛이 강해요");
+    private static final List<String> tagNames = List.of("더워요", "따뜻해요", "습해요", "추워요", "일교차가 커요", "선선해요", "쌀쌀해요", "건조해요", "바람이 불어요", "햇빛이 강해요");
     private final TagRepository tagRepository;
 
 //    @PostConstruct
